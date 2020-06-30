6a1a266cca2ca1f3d7202bb5e18a6c7723404ad2
NonCover <3471949741@qq.com>
2020-06-30 14:33:44 +0800

    修改程序BUG

--- 堆排序/heapSort.py
+++ 堆排序/heapSort.py
@@ -2,44 +2,50 @@
 # @time：2020年1月17日
 # @url：https://www.bilibili.com/video/av47196993   参考教程
  
-def swap(arrs, i, j):
+def swap(arr, i, j):
      # 交换数组中两个值
-    stemp = arrs[i]
-    arrs[i] = arrs[j]
-    arrs[j] = stemp
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
 
-# 创建大顶堆 
-def heapify(arrs, i, length):
-    if i > length:
-        return
-    leftChild = 2 * i + 1   # 左子节点
-    rightChild = 2 * i + 2  # 右子节点
-    maxNode = i
-    if leftChild <= length:
-        if arrs[leftChild] > arrs[maxNode]:
-            maxNode = leftChild   # 最大节点等于左子节点
-    if rightChild <= length:
-        if arrs[rightChild] > arrs[maxNode]:
-            maxNode = rightChild  # 最大节点等于右子节点
-    if arrs[maxNode] != arrs[i]:    
-        swap(arrs, i, maxNode)  # 交换父节点和 最大值
-        heapify(arrs, maxNode, length)
+def heapify(arr, n, i):
+    """
+    构建堆得过程
+    """
+    if i >= n: return
+    c1 = 2 * i + 1
+    c2 = 2 * i + 2
+    mx = i
+    if c1 < n and arr[c1] > arr[mx]:
+        mx = c1
+    if c2 < n and arr[c2] > arr[mx]:
+        mx = c2
+    if mx !=  i:
+        swap(arr, mx, i)
+        heapify(arr, n, mx)
 
-def build_heap(arrs, length):
-    for i in range( (length - 1) // 2 , -1, -1):
-        # print(arrs)
-        heapify(arrs, i, length - 1)
-    swap(arrs, 0, length - 1)
+def heap_sort(arr, n):
+    """
+    每次heapify后，将堆顶元素与最后一个交换，下次计算就不用计算最后一个，每次长度减 1
+    """
+    build_heap(arr, n)
+    for i in range(n - 1, -1, -1):
+        swap(arr, i, 0)         ## 堆顶拿出来
+        build_heap(arr, i)
 
-def heapsort(arrs):
-    for i in range( len(arrs), 2, -1 ):
-        build_heap(arrs, i)
-        # print(arrs, i)
+def build_heap(arr, n):
+    """
+    从非叶子节点开始，从下到上，从右到左依次构建堆
+    """
+    last = n - 1
+    parent = (last - 1) // 2
+    for i in range(parent, -1, -1):
+        heapify(arr, n, i)
 
-def main(arr):
-    heapsort(arr)    
-    print(arr)
 
 if __name__ == "__main__":
     # 完整二叉树:
-    main([4, 2, 6, 8, 3, 10, 1, 0, 7, 5])
+    arr = [6, 7, 4, 8, 1, 10, 5]
+    n = len(arr)
+    heap_sort(arr, n)
+    print(arr)
